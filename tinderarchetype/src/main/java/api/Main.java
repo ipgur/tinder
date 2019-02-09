4f8f85dda332b33fbcc21f315e0c5e3089bc5a0a
Raffaele Ragni <raffaele.ragni@gmail.com>
2019-02-09 17:47:06 +0100

    added auto swagger

--- tinderarchetype/src/main/java/api/Main.java
+++ tinderarchetype/src/main/java/api/Main.java
@@ -6,7 +6,10 @@ import static spark.Spark.staticFiles;
 
 public class Main {
   public static void main(String[] args) {
+    // Swagger will be available on the root, by static files in docs resources.
     staticFiles.location("/docs");
+    // Bind all your generated resources here...
+    // Patter is Resource<Name>.bind(new <Name>());
     ResourceExample.bind(new Example());
   }
 }
