795c4bb08642c8907f8d2eecf1a452aa0a601ee2
Raffaele Ragni <raffaele.ragni@verizonmedia.com>
2019-02-28 10:56:59 +0100

    cover configuration and module

---
+++ tinder/core/src/test/java/tinder/core/modules/TinderConfigurationTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Raffaele Ragni.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tinder.core.modules;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Raffaele Ragni
+ */
+public class TinderConfigurationTest {
+
+  @Test
+  public void testConfigurationDefaultValues() {
+    // Keep this test to ensure that default values don't get changed naively
+    TinderConfiguration configuration = ImmutableTinderConfiguration.builder()
+        .build();
+
+    Assertions.assertEquals(true, configuration.useSpark());
+    Assertions.assertEquals(8080, configuration.sparkPort());
+    Assertions.assertEquals(-1, configuration.sparkMinThreads());
+    Assertions.assertEquals(-1, configuration.sparkMaxThreads());
+    Assertions.assertEquals(-1, configuration.sparkIdleTimeoutMillis());
+    Assertions.assertEquals("jdbi", configuration.jdbiInstanceName());
+  }
+
+}
--- tinder/core/src/test/java/tinder/core/spark/RoutesTest.java
+++ tinder/core/src/test/java/tinder/core/spark/RoutesTest.java
@@ -13,14 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package tinder.core.auth;
+package tinder.core.spark;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import org.jdbi.v3.core.Jdbi;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import spark.Spark;
-import tinder.core.JDBILoader;
+import tinder.core.auth.AuthenticationFilter;
+import tinder.core.auth.AuthenticationResources;
+import tinder.core.modules.ImmutableTinderConfiguration;
+import tinder.core.modules.TinderConfiguration;
+import tinder.core.modules.TinderModule;
 
 /**
  *
@@ -29,11 +34,16 @@ import tinder.core.JDBILoader;
 public class RoutesTest {
   @Test
   public void testRoutes() {
-    Jdbi jdbi = JDBILoader.load();
-    // Test with the spark endpoints now
 
-    // Use random available port
-    Spark.port(0);
+    // Keep all spark tests in this test, so that it won't clash with itself with the inner signleton.
+
+    TinderConfiguration configuration = ImmutableTinderConfiguration.builder().build();
+    TinderModule module = new TinderModule(configuration);
+    Assertions.assertEquals(configuration, module.configuration());
+
+    Jdbi jdbi = module.jdbi(configuration);
+
+    // Test with the spark endpoints now
 
     AuthenticationFilter.addAPIBasedFilter("/auth1/*", "http://localhost:4567");
     AuthenticationFilter.addAPIBasedFilter("/auth1-a/*", "http://localhost:4567", new HashSet(Arrays.asList("/myendpoint")));
