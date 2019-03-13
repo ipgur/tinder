c54840c6c4b08bc1e9662ac8a39fb8fa57c707d5
Raffaele Ragni <raffaele.ragni@verizonmedia.com>
2019-03-13 09:19:50 +0100

    need annotation to seralize immutables

--- tinderarchetype/src/main/java/api/model/Bean.java
+++ tinderarchetype/src/main/java/api/model/Bean.java
@@ -1,11 +1,13 @@
 package api.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import javax.annotation.Nullable;
-import org.immutables.gson.Gson;
 import org.immutables.value.Value.Immutable;
 
 @Immutable
-@Gson.TypeAdapters
+@JsonSerialize(as = ImmutableBean.class)
+@JsonDeserialize(as = ImmutableBean.class)
 public interface Bean {
   @Nullable Long id();
   @Nullable Long limit();
